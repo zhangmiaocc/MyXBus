b7204e68f50d1c0b379559ce104569d8d2c24c35
zm <479326215@qq.com>
2018-08-21 17:47:13 +0800

    no message

---
+++ app/src/main/java/com/zm/myxbus/MainActivity.java
@@ -0,0 +1,74 @@
+package com.zm.myxbus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.mcxiaoke.bus.Bus;
+import com.mcxiaoke.bus.annotation.BusReceiver;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 注册
+        Bus.getDefault().register(this);
+
+        /**
+         *  BusEvent: String event
+         *  BusEvent: Object event
+         *  顺序不固定
+         */
+        Bus.getDefault().post("String Event");
+
+        /**
+         *  BusEvent: Object event
+         *  BusEvent: SomeEvent event
+         *  顺序不固定
+         */
+        Bus.getDefault().post(new SomeEvent());
+
+        /**
+         *  BusEvent: Object event
+         */
+        Bus.getDefault().post(new Object());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 取消注册
+        Bus.getDefault().unregister(this);
+    }
+
+    /**
+     *  handle your event
+     *  这里处理事件
+     */
+    @BusReceiver
+    public void onStringEvent(String event) {
+        Log.d("BusEvent", "String event");
+    }
+
+    /**
+     *  SomeEventClass表示任意的自定义类
+     *  handle your event
+     *  这里处理事件
+     */
+    @BusReceiver
+    public void onSomeEvent(SomeEvent event) {
+        Log.d("BusEvent", "SomeEvent event");
+    }
+
+    /**
+     *  不建议使用Object，会收到所有类型的事件
+     *  handle your event
+     *  这里处理事件
+     */
+    @BusReceiver
+    public void onObjectEvent(Object event) {
+        Log.d("BusEvent", "Object event");
+    }
+}
---
+++ app/src/main/java/com/zm/myxbus/SomeEvent.java
@@ -0,0 +1,9 @@
+package com.zm.myxbus;
+
+/**
+ * Created by zm on 2018/8/21.
+ */
+
+public class SomeEvent {
+
+}
